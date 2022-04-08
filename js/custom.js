fa6b93c6dbfaebca83d6e8348a2abc8ffedd1687
HoangYen249 <97886323+HoangYen249@users.noreply.github.com>
2022-04-08 20:25:21 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+new WOW().init();
+var typed = new Typed('.typing', {
+     strings: ["phóng thôi."],
+     typeSpeed:50,
+     backSpeed:50,
+     loop:true,
+     });
+var typed = new Typed('.typing1', {
+     strings: ["hãy cùng mình lượn 1 vòng <br> khám phá vũ trụ thiết kế <br>của mình nhé."],
+     typeSpeed:50,
+     backSpeed:50,
+     loop:true,
+     });
